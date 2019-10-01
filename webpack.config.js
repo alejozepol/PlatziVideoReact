a75d8a2b0992b79ba49d76848cb1d433934aa1d1
Alejandro Lopez <alejozepol@gmail.com>
2019-09-30 22:31:49 -0500

    Instalación para utilización de sass (preprocesador css)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtraxtPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry : './src/index.js',
@@ -24,13 +25,26 @@ module.exports = {
                 use: {
                     loader: 'html-loader'
                     }
-            }
+            },
+            {
+                test: /\.(s*)css$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: MiniCssExtraxtPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
         ]
     },
     plugins:[
         new HtmlWebPackPlugin({
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtraxtPlugin({
+            filename: 'assets/[name].css'
         }),
     ]
 
