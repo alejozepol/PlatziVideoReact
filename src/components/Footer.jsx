441e247dfbf0b827414541d94abb5b2ac04a8066
Alejandro Lopez <alejozepol@gmail.com>
2019-10-03 16:52:16 -0500

    componenete footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/components/Footer.scss';
+
+const Footer = () => (
+  <footer>
+    <a href='x'>Terminos de uso</a>
+    <a href='x'>Declaración de privacidad</a>
+    <a href='x'>Centro de ayuda</a>
+  </footer>
+);
+
+export default Footer;
