dab63e52f687cfd8c7ccf482a79a632a1cae771b
Alejandro Lopez <alejozepol@gmail.com>
2019-10-03 18:14:38 -0500

    creacion de hooks para la utilizacion de apis

---
+++ src/hooks/UseInitialState.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+const UseInitialSatete = (API) => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+  return videos;
+};
+
+export default UseInitialSatete;
