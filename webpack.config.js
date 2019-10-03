912d59ff1aabac3543482e6276afcf6942a601b0
Alejandro Lopez <alejozepol@gmail.com>
2019-10-03 16:53:23 -0500

    instalacion de manejo de archivos con webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,58 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin'); 
-const MiniCssExtraxtPlugin = require('mini-css-extract-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtraxtPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry : './src/index.js',
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
-    },
-    resolve:{
-        extensions: ['.js', '.jsx']
-    },
-    module:{
-        rules:[
-            {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-                }
-            },
-            {
-                test:/\.html$/,
-                use: {
-                    loader: 'html-loader'
-                    }
-            },
-            {
-                test: /\.(s*)css$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: MiniCssExtraxtPlugin.loader,
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-        ]
-    },
-    plugins:[
-        new HtmlWebPackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtraxtPlugin({
-            filename: 'assets/[name].css'
-        }),
-    ]
-
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [{
+          loader: MiniCssExtraxtPlugin.loader,
+        },
+        'css-loader',
+        'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'assets/[hash].¨[ext]',
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtraxtPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
 };
