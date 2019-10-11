4929fe959f6bc6027505c591a025f12eb6c5fa6f
Alejandro Lopez <alejozepol@gmail.com>
2019-10-11 00:02:09 -0500

    implementacion de gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/ico-perfil.png';
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <img className='header__img' src={logo} alt='LogoPlatziVideo' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--perfil'>
-        <p>Perfil</p>
-        <img className='menu__perfil--perfil' src={userIcon} alt='Icono Perfil' />
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='LogoPlatziVideo' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--perfil'>
+          <p>Perfil</p>
+          {hasUser ? (
+            <img
+              className='menu__perfil--perfil'
+              src={gravatar(user.email)}
+              alt={user.email}
+            />
+          ) : (
+            <img
+              className='menu__perfil--perfil'
+              src={userIcon}
+              alt='Icono Perfil'
+            />
+          )}
+        </div>
+        <ul>
+          <li><a href='/'>Cuenta</a></li>
+          <li>
+            <Link to='/login'>
+              Iniciar Sesión
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li><a href='/'>Cuenta</a></li>
-        <li>
-          <Link to='/login'>
-            Iniciar Sesión
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
+
