af4ab1ae179bfbddd7214909672eb74f9c80a745
Alejandro Lopez <alejozepol@gmail.com>
2019-11-01 15:18:10 -0500

    configuracion para server webpack desde express

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dovenv from 'dotenv';
+import webpack from 'webpack';
 
 dovenv.config();
 
@@ -7,6 +8,24 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+if (ENV === 'development') {
+  console.log('Ejecucion en entorno de desarrollo');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = {
+    contentBase: `http://localhost:${PORT}`,
+    port: PORT,
+    publicPath: webpackConfig.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: { colors: true },
+  };
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.get('*', (req, res) => {
   res.send({ holaMundo: true });
