6213df694752edcff12960e8d84bc1920ca09c1f
Alejandro Lopez <alejozepol@gmail.com>
2019-11-01 14:37:08 -0500

    instalacion de paquetes eslint-loader react-hot-loader autoprefixer babel-plugin-transform-class-properties babel-plugin-transform-object-assign postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtraxtPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/frontend/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath:'/',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: {
+          loader: 'eslint-loader',
+        },
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -34,6 +44,7 @@ module.exports = {
         },
         'css-loader',
         'sass-loader',
+        'postcss-loader',
         ],
       },
       {
@@ -51,6 +62,13 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer(),
+        ],
+      },
+    }),
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html',
